718f8adc0418d2ce03fe9d903727969ff099b1cd
Rudolf <rudolfcickodev@gmail.com>
2020-11-14 17:43:37 +0100

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+import './assets/scss/style.scss'
+
+
+const App = () => {
+
+    return <div>Hola amigo mio</div>
+}
+
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
+)
